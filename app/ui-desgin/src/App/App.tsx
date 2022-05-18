46479d726ec1472b241a70f5a4aa49b693bd320b
LeomharT <leo.liao.lzy@gmail.com>
2022-05-18 14:40:08 +0800

    设计和实验分开哦

--- app/ui-desgin/src/App/App.tsx
+++ app/ui-desgin/src/App/App.tsx
@@ -1,28 +1,27 @@
-import { FocusStyleManager } from '@blueprintjs/core';
-import React, { Component } from 'react';
+import { ColorScheme } from '@mantine/core';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Mantine from '../mantine/mantine';
 import RectToBoard from '../R2B_Model/components/ReacToBoard/RectToBoard';
 
-export default class App extends Component<{}, {}>
+export default function App()
 {
-    constructor(props: {})
-    {
-        super(props);
-        FocusStyleManager.onlyShowFocusOnTabs();
-    }
-    render()
-    {
-        return (
-            <div className='App'>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path='/' element={<div>hello</div>} />
-                        <Route path='/r2bbord' element={<RectToBoard />} />
-                        <Route path='/mantine' element={<Mantine />} />
-                    </Routes>
-                </BrowserRouter>
-            </div>
-        );
-    }
+    const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+    return (
+
+        <div className='App'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<div>hello</div>} />
+                    <Route path='/r2bbord' element={<RectToBoard />} />
+                    {/** 现在没有模糊匹配了exact */}
+                    {/* <Route path='/mantine' element={<Mantine />} />
+                        <Route path='/mantine/:id' element={<Mantine />} /> */}
+                    {/* 可以写成嵌套的模式 */}
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
--- app/ui-desgin/src/mantine/mantine.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Mantine()
-{
-    return (
-        <div>giao</div>
-    );
-}
