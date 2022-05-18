d43a5c7d6cca2a4abf909f4dfada63430ccbbd5b
LeomharT <leo.liao.lzy@gmail.com>
2022-05-18 15:24:55 +0800

    Record 联合类型为key,第二类型为类型

--- app/es6/@types/index.ts
+++ app/es6/@types/index.ts
@@ -10,3 +10,31 @@ export const Intent = {
 
 type Intent = typeof Intent['DANGER'];
 export * as Classes from './classes';
+
+interface A
+{
+    cat: string;
+    dog: string;
+    rat: string;
+}
+interface B
+{
+    name: string;
+    age: string;
+}
+
+let aa: Record<keyof A, B>;
+aa = {
+    cat: {
+        name: '',
+        age: ''
+    },
+    dog: {
+        name: '',
+        age: ''
+    },
+    rat: {
+        name: '',
+        age: ''
+    },
+};
