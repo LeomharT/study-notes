9f892d8de7e2cb70f4b76ed03a2369a4d5406f52
LeomharT <leo.liao.lzy@gmail.com>
2022-05-18 09:58:52 +0800

    install mantine

--- app/ui-desgin/src/App/App.tsx
+++ app/ui-desgin/src/App/App.tsx
@@ -1,5 +1,7 @@
 import { FocusStyleManager } from '@blueprintjs/core';
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Mantine from '../mantine/mantine';
 import RectToBoard from '../R2B_Model/components/ReacToBoard/RectToBoard';
 
 export default class App extends Component<{}, {}>
@@ -13,7 +15,13 @@ export default class App extends Component<{}, {}>
     {
         return (
             <div className='App'>
-                <RectToBoard />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<div>hello</div>} />
+                        <Route path='/r2bbord' element={<RectToBoard />} />
+                        <Route path='/mantine' element={<Mantine />} />
+                    </Routes>
+                </BrowserRouter>
             </div>
         );
     }
