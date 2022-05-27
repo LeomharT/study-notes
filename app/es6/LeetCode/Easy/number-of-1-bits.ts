645863c8786894444f87ec4d315d9856910bb903
LeomharT <leo.liao.lzy@gmail.com>
2022-05-27 10:09:14 +0800

    众所周知名字越长越垃圾

--- app/es6/LeetCode/Easy/number-of-1-bits.ts
+++ app/es6/LeetCode/Easy/number-of-1-bits.ts
@@ -3,6 +3,4 @@ function hammingWeight(n: number): number
     return n.toString(2).split('').filter(v => v == '1').length;
 };
 
-
-
 console.log(hammingWeight(parseInt('00000000000000000000000000001011')));
---
+++ app/es6/LeetCode/Easy/number-of-steps-to-reduce-a-number-to-zero.ts
@@ -0,0 +1,17 @@
+function numberOfSteps(num: number): number
+{
+    let step: number = 0;
+
+    while (num > 0)
+    {
+        if (num % 2 === 0) num = num / 2;
+        else num--;
+        step++;
+    }
+
+    return step;
+};
+
+
+
+console.log(numberOfSteps(14));
