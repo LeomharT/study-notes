ea81522113bfdf7835531f7ce3b5d3899f180fba
LeomharT <leo.liao.lzy@gmail.com>
2022-06-05 19:05:50 +0800

    build

--- app/node-google-translate-azure-voice/rollup.config.js
+++ app/node-google-translate-azure-voice/rollup.config.js
@@ -1,6 +1,21 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
+
 export default {
-
-
-
-    file: './bundle.mjs'
-}
+    plugins: [
+        commonjs(),
+        json(),
+        nodeResolve({ preferBuiltins: true }),
+        typescript({
+            exclude: "node_modules/**",
+            typescript: require("typescript")
+        })
+    ],
+    input: './src/index.ts',
+    output: {
+        file: './bundle.mjs',
+        format: 'es'
+    }
+};
