991d69ecf77613a7582243c8b49decdfd70efd40
LeomharT <leo.liao.lzy@gmail.com>
2022-06-06 14:37:16 +0800

    start https

--- app/node-google-translate-azure-voice/src/index.ts
+++ app/node-google-translate-azure-voice/src/index.ts
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import https from 'https';
 import App from './app/app.js';
 import './trans-voice.js';
 import './trans-word.js';
 
-App.StartServe();
+const { app } = App.GetInstance();
+
+https.createServer({
+    key: fs.readFileSync('./7392576_www.liaozhengyang.xyz.key'),
+    cert: fs.readFileSync('./7392576_www.liaozhengyang.xyz.pem'),
+}, app).listen(626, () =>
+{
+    console.log(`\u001b[1;34mApp Running Successfully on port 626!!\u001b[0m`);
+});
