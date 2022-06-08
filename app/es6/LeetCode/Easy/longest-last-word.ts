c8e4132b0ee60c9149fce9345053c0641ba57b91
LeomharT <leo.liao.lzy@gmail.com>
2022-06-08 09:27:44 +0800

    regexp

---
+++ app/es6/LeetCode/Easy/longest-last-word.ts
@@ -0,0 +1,15 @@
+function lengthOfLastWord(s: string): number
+{
+    s = s.replace(/( )\1+/g, '$1').trimStart().trimEnd();
+
+    console.log(s);
+
+    const words = s.split(" ");
+
+    return words[words.length - 1].length;
+};
+
+
+
+
+console.log(lengthOfLastWord("Today is a nice day"));
