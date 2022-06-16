60fe1ffec4ed8a61835d7a733927df6072bebc35
LeomharT <leo.liao.lzy@gmail.com>
2022-06-16 18:23:42 +0800

    改为安全的https地址

--- app/google-plugin/assets/js/index.js
+++ app/google-plugin/assets/js/index.js
@@ -2,7 +2,7 @@
  *  ✨antd.Button是对象啊,不是原始Element类型谢谢!✨xe
  */
 
-const HOST = 'https://47.107.42.46:626';
+const HOST = 'https://liaozhengyang.zyz:626';
 
 const RequestURL = {
     trans_word: HOST + '/TransWord',
