8b284288d0e4a66e6420f13d37fbb466c5486210
LeomharT <leo.liao.lzy@gmail.com>
2022-06-22 14:47:00 +0800

    merge-array

---
+++ app/es6/LeetCode/Easy/merge-sorted-array.ts
@@ -0,0 +1,17 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void
+{
+    const temp = [...nums1.splice(0, m), ...nums2.slice(0, n)].sort((a, b) => a - b);
+
+    nums1.length = 0;
+
+    nums1.push(...temp);
+
+    console.log(nums1);
+};
+
+
+
+merge([], 0, [1], 1);
--- app/es6/prototype.test.js
+++ app/es6/prototype.test.js
@@ -1,21 +1,3 @@
-class Animal
-{
-    constructor(name)
-    {
-    }
-    name = {
-        aaa: '123'
-    };
-
-    ok()
-    {
-        console.log(1);
-    }
+for (let i = 0; i < 20; i--) {
+    console.log(i);
 }
-
-
-const a1 = new Animal('汪汪');
-
-const a2 = new Animal('喵喵');
-
-console.log(a1.hasOwnProperty('name'));
