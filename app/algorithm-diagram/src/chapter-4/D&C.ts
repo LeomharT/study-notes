e63bad75a78a503c9d096f0491f5694bb8e6f5e0
LeomharT <leo.liao.lzy@gmail.com>
2022-06-22 14:46:16 +0800

    bibao

--- app/algorithm-diagram/src/chapter-4/D&C.ts
+++ app/algorithm-diagram/src/chapter-4/D&C.ts
@@ -23,3 +23,56 @@ function sum(numbers: number[]): number
 
     return numbers.pop() as number + sum(numbers);
 }
+
+
+function _length(numbers: number[]): number
+{
+    if (numbers.length === 1) return 1;
+
+    numbers.pop();
+
+    return 1 + _length(numbers);
+}
+
+
+function quickSort(numbers: number[]): number[]
+{
+    if (numbers.length < 2) return numbers;
+
+    //基准值是几其实不重要
+    const pivolt = numbers[0];
+
+    const less: number[] = [];
+
+    const greater: number[] = [];
+
+    for (let i of numbers)
+    {
+        if (i <= pivolt) less.unshift(i);
+
+        if (i > pivolt) greater.unshift(i);
+    }
+
+    const result = quickSort(less).concat(pivolt).concat(quickSort(greater));
+
+    return result;
+}
+
+// console.log(quickSort([2, 3, 1, 5, 8, 4, 6, 9, 7]));
+
+
+function fn1()
+{
+    let a = 1;
+    return function ()
+    {
+        return a++;
+    };
+}
+const x = fn1();
+
+console.log(x());
+console.log(x());
+console.log(x());
+console.log(x());
+console.log(x());
