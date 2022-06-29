3b51a2c15510dfcca787e843f98c8c17e4317e6c
LeomharT <leo.liao.lzy@gmail.com>
2022-06-29 22:35:55 +0800

    写代码的乐趣

--- app/es6/prototype.test.js
+++ app/es6/prototype.test.js
@@ -1,3 +1,14 @@
-for (let i = 0; i < 20; i--) {
-    console.log(i);
+const arr = [1, 2, 21, 4, 7, 0];
+
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
 }
+
+console.log(arr);
