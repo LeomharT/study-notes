0bf24b13d00569f0dbafdb3bb95951c2f68b2c17
LeomharT <leo.liao.lzy@gmail.com>
2022-07-25 10:00:22 +0800

    binary tree

---
+++ app/es6/LeetCode/Easy/binary-tree-inorder-traversal.ts
@@ -0,0 +1,31 @@
+class TreeNode
+{
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null)
+    {
+        this.val = (val === undefined ? 0 : val);
+        this.left = (left === undefined ? null : left);
+        this.right = (right === undefined ? null : right);
+    }
+}
+
+function inorderTraversal(root: TreeNode | null): number[]
+{
+    const res: number[] = [];
+
+    traverse(root, res);
+
+    return res;
+};
+function traverse(root: TreeNode | null, result: number[]): number[] | void
+{
+    if (!root) return [];
+
+    traverse(root.left, result);
+
+    result.push(root.val);
+
+    traverse(root.right, result);
+}
