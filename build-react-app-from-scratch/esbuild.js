723e60506e98d9508375ac6c70803f9b120b1d28
LeomharT <leo.liao.lzy@gmail.com>
2022-09-02 12:12:53 +0800

    [更新]->watch即为热更

--- build-react-app-from-scratch/esbuild.js
+++ build-react-app-from-scratch/esbuild.js
@@ -1,1 +1,8 @@
 const esbuild = require('esbuild');
+
+esbuild.build({
+    entryPoints: ['index.tsx'],
+    bundle: true,
+    outfile: './public/bundle.js',
+    watch: true,
+}).catch(err => { console.log(err); });
