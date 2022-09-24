788abc188c53c318b267b5b88270a036532e1b3d
LeomharT <leo.liao.lzy@gmail.com>
2022-09-24 23:01:19 +0800

    [更新]->添加加载组件

---
+++ ui-desgin/src/feature/spinner/spinner.tsx
@@ -0,0 +1,8 @@
+export default function spinner()
+{
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <circle cx="25" cy="25" r="20" stroke="black" strokeWidth="2" fill="none" />
+        </svg>
+    );
+}
