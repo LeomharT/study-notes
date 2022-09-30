67fc0d02d9b12661306c03fef0ead873e30374cc
LeomharT <leo.liao.lzy@gmail.com>
2022-09-30 15:04:55 +0800

    🎉[New]->Add Hanburgers demo

--- ui-desgin/src/feature/hamburger/index.tsx
+++ ui-desgin/src/feature/hamburger/index.tsx
@@ -1,21 +1,31 @@
+import { FocusStyleManager } from '@blueprintjs/core';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
 import classnames from 'classnames';
-import { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './scss/index.scss';
 
-export function HamburgerButton()
-{
-    const [isExpanded, setIsExpanded] = useState(false);
+export type HamburgerButtonProps = {
+    className?: string;
+    isExpanded?: boolean;
+    onClick?: (e: React.MouseEvent) => any;
+};
 
+FocusStyleManager.onlyShowFocusOnTabs();
+
+export function HamburgerButton(props: HamburgerButtonProps)
+{
     const className = useMemo(() =>
     {
         return classnames(
             'hamburger',
             'hamburger--elastic',
             {
-                'is-active': isExpanded
-            }
+                'is-active': props.isExpanded
+            },
+            props.className
         );
-    }, [isExpanded]);
+    }, [props.isExpanded, props.className]);
 
 
     return (
@@ -25,9 +35,10 @@ export function HamburgerButton()
             aria-label="Menu"
             aria-controls="navigation"
             aria-expanded="true"
-            onClick={() =>
+            onClick={(e) =>
             {
-                setIsExpanded(prve => !prve);
+                if (props.onClick)
+                    props.onClick(e);
             }}
         >
             <span className="hamburger-box">
@@ -41,11 +52,56 @@ export function HamburgerButton()
 
 export default function Hamburger()
 {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    useEffect(() =>
+    {
+
+        //@ts-ignore
+        console.log($);
+    }, []);
+
     return (
-        <div className="hamburger">
-            <div>
-                <HamburgerButton />
-            </div>
+        <div className="hamburger-demo">
+            <header>
+                <HamburgerButton
+                    className='navi_btn'
+                    isExpanded={isExpanded}
+                    onClick={e =>
+                    {
+                        setIsExpanded(prve => !prve);
+                    }}
+                />
+            </header>
+            <main>
+                <div className='popover-navi' aria-expanded={isExpanded}>
+                    <div className="expander" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        一级菜单
+                    </div>
+                    <div className="collapse" id="collapseExample">
+                        <div className="well">
+                            <a href='#'>二级菜单</a>
+                        </div>
+                    </div>
+                    <div className="expander"
+                        data-toggle="collapse"
+                        data-target="#collapseExample2"
+                        aria-expanded="false"
+                        aria-controls="collapseExample2"
+                    >
+                        一级菜单
+                    </div>
+                    <div className="collapse" id="collapseExample2">
+                        <div className="well">
+                            <a href='#'>二级菜单1</a>
+                            <a href='#'>二级菜单2</a>
+                            <a href='#'>二级菜单3</a>
+                            <a href='#'>二级菜单4</a>
+                            <a href='#'>二级菜单5</a>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div >
     );
 }
